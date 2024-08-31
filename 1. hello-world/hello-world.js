1e8a7fef5e28dbfb2d170e0b0310faa4380c72a3
Juliana <jufarias13@gmail.com>
2024-08-31 15:00:56 -0300

    primeiros códigos do curso

---
+++ 1. hello-world/hello-world.js
@@ -0,0 +1,8 @@
+// pega o elemento com a id "resultado"
+const elemento = document.getElementById("resultado")
+
+// cria uma constante com a mensagem "Hello, World!"
+const mensagem = "Hello, World!";
+
+// altera o conteúdo do elemento para a mensagem
+elemento.textContent = mensagem;
---
+++ 2. variaveis/variaveis.js
@@ -0,0 +1,22 @@
+// variável armazenando uma string
+let nome;           // declaração da variável
+nome = "Jupiter";   // atribuição de valor
+
+// variável armazenando um número
+let idade;          // declaração da variável
+idade = 27;         // atribuição de valor
+let a, b, c;        // declaração de múltiplas variáveis
+a = 1;              // atribuição de valor
+b = 2;              // atribuição de valor
+c = 3;              // atribuição de valor
+soma = a + b + c;   // soma dos valores
+
+// variável armazenando um booleano
+let chuva;          // declaração da variável
+chuva = true;       // atribuição de valor
+
+// print das variáveis no console
+console.log(nome);  // Jupiter
+console.log(idade); // 27
+console.log(soma);  // 6
+console.log(chuva); // true
