c56825ea09ca801abf14636f3c319f4926829579
Juliana <jufarias13@gmail.com>
2024-09-01 09:57:38 -0300

    adição de código com if else

---
+++ 3. if-else/if-else.js
@@ -0,0 +1,10 @@
+// declaração da variável idade
+const idade = 20;
+
+// se a idade for maior ou igual a 18, imprime "Maior de idade"
+if (idade >= 18) {
+    console.log("Maior de idade");
+// senão, imprime "Menor de idade"
+} else {
+    console.log("Menor de idade");
+}
