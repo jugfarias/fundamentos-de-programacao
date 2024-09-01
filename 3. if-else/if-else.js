0e486de0c7dccfdd220f732d2562b50209c73643
Juliana <jufarias13@gmail.com>
2024-09-01 10:07:50 -0300

    adiçao de if dentro de if

--- 3. if-else/if-else.js
+++ 3. if-else/if-else.js
@@ -8,3 +8,21 @@ if (idade >= 18) {
 } else {
     console.log("Menor de idade");
 }
+
+// if dentro do if (usar no máximo 3)
+idade = 19;
+possuiCarteira = false;
+// se a idade for maior ou igual a 18, imprime "Maior de idade"
+if (idade >= 18) {
+    console.log("Maior de idade");
+    // se possui carteira, imprime "Pode dirigir"
+    if (possuiCarteira) {
+        console.log("Pode dirigir");
+    // senão, imprime "Não pode dirigir"
+    } else {
+        console.log("Não pode dirigir");
+    }
+// senão, imprime "Menor de idade"
+} else {    
+    console.log("Menor de idade");
+}
