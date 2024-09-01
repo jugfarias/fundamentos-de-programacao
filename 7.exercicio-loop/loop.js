3051d98a42730e917158619ecf235e8abfb98d20
Juliana <jufarias13@gmail.com>
2024-09-01 11:24:51 -0300

    exericio utilizando loops

---
+++ 7.exercicio-loop/loop.js
@@ -0,0 +1,23 @@
+// imprimir números de 1 a 10 usando loops
+
+// for
+console.log("For:");
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
+
+// while
+console.log("While:");  
+let i = 1;
+while (i <= 10) {
+    console.log(i);
+    i++;
+}
+
+// do-while
+console.log("Do-While:");
+i = 1;
+do {
+    console.log(i);
+    i++;
+} while (i <= 10);
