0408762d2e11b0473f13ef6b323dd92902bde626
Juliana <jufarias13@gmail.com>
2024-09-01 10:41:57 -0300

    adição de código com exemplos de estruturas de repeticao: for, while e do-while

---
+++ 4. repeticao/repeticao.js
@@ -0,0 +1,20 @@
+// printar os números de 0 a 4 no console com for, while e do-while
+
+// for
+for (let i = 0; i < 5; i++) {
+    console.log(i);
+}
+
+// while
+let i = 0;
+while (i < 5) {
+    console.log(i);
+    i++;
+}
+
+// do-while
+i = 0;
+do {
+    console.log(i);
+    i++;
+} while (i < 5);
