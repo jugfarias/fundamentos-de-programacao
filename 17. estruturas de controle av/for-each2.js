3546d509221f87a9fd590e424b2b6f688ca2449a
jugfarias <jufarias13@gmail.com>
2024-09-06 15:57:41 -0300

    exemplo mais simples de for each

---
+++ 17. estruturas de controle av/for-each2.js
@@ -0,0 +1,9 @@
+// exemplo simples de for each
+
+// cria um array
+const frutas = ["banana", "maçã", "uva", "pera", "melancia"];
+
+// para cada fruta no array de frutas, imprime a fruta
+frutas.forEach(function(fruta) {
+    console.log(fruta);
+});
